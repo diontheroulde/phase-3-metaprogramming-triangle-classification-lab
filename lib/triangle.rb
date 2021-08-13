90971762ecd724ffd4c2990f15bf02fd126296d8
Dion Theroulde <dionjr105@gmail.com>
2021-08-13 09:34:30 -0400

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require "pry"
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+   @a = a
+   @b = b
+   @c = c
+  end
+
+  def kind
+    check_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def side_greater_than_zero?
+    a > 0 && b > 0 && c > 0
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def check_triangle
+    raise TriangleError unless side_greater_than_zero? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
